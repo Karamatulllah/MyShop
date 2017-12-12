59ad4a06da7b5aeaf8b14103811ebe4559028b14
Khurram Wasim <khurram0403@gmail.com>
2017-12-12 11:33:58 +0500

    initial commit

---
+++ listProducts.js
@@ -0,0 +1,11 @@
+var faker = require("faker");
+console.log("===================================");
+console.log("        Welcome to My Shop");
+console.log("===================================");
+
+for (var i = 0; i < 10; i++) {
+  var prdName = faker.commerce.productName();
+  var prdPrice = faker.commerce.price();
+  console.log(prdName + "  -  $" + prdPrice);
+
+}
